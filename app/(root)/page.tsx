260d42930adb2d4f29646eadd4fc084f009e65ba
Aliasgar <aliasgardf@gmail.com>
2023-07-28 21:18:04 +0530

    Added Form Components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -8,7 +8,7 @@ import { useEffect } from "react";
 
 const SetupPage = () => {
     const onOpen = useStoreModal((state) => state.onOpen);
-    const isOpen = useStoreModal((state) => state.onOpen);
+    const isOpen = useStoreModal((state) => state.isOpen);
 
     useEffect(() => {
         if (!isOpen) {
